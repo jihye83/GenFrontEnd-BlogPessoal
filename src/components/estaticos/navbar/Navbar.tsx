955bb3db4e0a4e7e814224ab07663c17335100ad
ji hye <jihye83@hotmail.com>
2022-11-01 14:48:48 -0300

    adicionando componente Login

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { AppBar, Toolbar, Typography} from "@material-ui/core";
-import { Box } from '@mui/material';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
@@ -34,16 +36,18 @@ function Navbar() {
                 cadastrar tema
               </Typography>
             </Box>
-            <Box mx={1} style={{ cursor: "pointer" }}>
-              <Typography variant="h6" color="inherit">
-                logout
-              </Typography>
-            </Box>
+            <Link to="/login" className="text-decorator-none">
+              <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
+                <Typography variant="h6" color="inherit">
+                  logout
+                </Typography>
+              </Box>
+            </Link>
           </Box>
         </Toolbar>
       </AppBar>
     </>
-  )
+  );
 }
 
 export default Navbar;
