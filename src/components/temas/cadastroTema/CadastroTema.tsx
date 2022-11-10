c239b2cd2dd1d655af2d548670ed576d88605f30
ji hye <jihye83@hotmail.com>
2022-11-10 09:54:56 -0300

    tema  post, put and delete

--- src/components/temas/cadastroTema/CadastroTema.tsx
+++ src/components/temas/cadastroTema/CadastroTema.tsx
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Button, Container, TextField, Typography } from '@material-ui/core';
 import Tema from '../../../models/Tema';
 import './CadastroTema.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { buscaId, post, put } from '../../../services/Service';
 
 function CadastroTema() {
+    let history = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const [token, setToken] = useLocalStorage('token');
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        descricao: ''
+    })
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado!")
+            history('/login')
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedTema(event: ChangeEvent<HTMLInputElement>) {
+        setTema({
+            ...tema,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    async function onSubmit(event: ChangeEvent<HTMLFormElement>) {
+        event.preventDefault()
+        console.log("tema " + JSON.stringify(tema))
+        if (id !== undefined) {
+            console.log(tema)
+            put(`/temas`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema atualizado com sucesso!');
+        } else {
+            post(`/temas`, tema, setTema, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Tema cadastrado com sucesso!');
+        }
+        back()
+    }
+
+    function back() {
+        history('/temas')
+    }
 
     return (
         <Container maxWidth="sm" className="topo">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
-                <TextField id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <TextField value={tema.descricao} onChange={(event: ChangeEvent<HTMLInputElement>) => updatedTema(event)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
                 <Button type="submit" variant="contained" color="primary">
                     Finalizar
                 </Button>
