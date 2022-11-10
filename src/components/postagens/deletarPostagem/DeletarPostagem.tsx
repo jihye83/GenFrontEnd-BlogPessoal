4cabb841df1a57e154b3506d2449f65efbdd90b5
ji hye <jihye83@hotmail.com>
2022-11-10 14:39:48 -0300

    cadastro postagem, modelPostagem

--- src/components/postagens/deletarPostagem/DeletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
 import { Box } from '@mui/material'
 
 import Postagem from '../../../models/Postagem';
 import './DeletarPostagem.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { buscaId, deleteId } from '../../../services/Service';
 
 function DeletarPostagem() {
+    let history = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const [token, setToken] = useLocalStorage('token');
+    const [postagem, setPostagem] = useState<Postagem>()
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado!")
+            history('/login')
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/postagens/${id}`, setPostagem, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function sim() {
+        history('/postagens')
+        deleteId(`/postagens/${id}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        alert('Postagem deletado co seucesso!');
+    }
+
+    function nao() {
+        history('/postagens')
+    }
+
+
     return (
         <>
             <Box m={2}>
@@ -16,7 +60,7 @@ function DeletarPostagem() {
                                 Deseja deletar a Postagem:
                             </Typography>
                             <Typography color="textSecondary" >
-                                Tema
+                                {postagem?.titulo}
                             </Typography>
                         </Box>
 
@@ -24,12 +68,12 @@ function DeletarPostagem() {
                     <CardActions>
                         <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
                             <Box mx={2}>
-                                <Button variant="contained" className="marginLeft" size='large' color="primary">
+                                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
                                     Sim
                                 </Button>
                             </Box>
                             <Box>
-                                <Button variant="contained" size='large' color="secondary">
+                                <Button onClick={nao} variant="contained" size='large' color="secondary">
                                     Não
                                 </Button>
                             </Box>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Typography, Grid, Button } from "@material-ui/core";
 import { Box } from "@mui/material";
 import TabPostagem from "../../components/postagens/tabpostagens/TabPostagem";
 import "./Home.css";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import useLocalStorage from "react-use-localstorage";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  let history = useNavigate();
+  const [token, setToken] = useLocalStorage('token');
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado!")
+      history("/login")
+
+    }
+  }, [token])
+
   return (
     <>
       <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa" >
@@ -18,7 +32,9 @@ function Home() {
             </Typography>
           </Box>
           <Box display="flex" justifyContent="center">
-            <Box marginRight={1}></Box>
+            <Box marginRight={1}>
+              <ModalPostagem />
+            </Box>
             <Button variant="outlined" className="botao">
               Ver Postagens
             </Button>
